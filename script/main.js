3809f746edb6735663b6e39a4608c5e18ad52cd2
Deepankur <deepankur.rastogi1995@gmail.com>
2019-11-12 21:48:24 +0530

    changes in screensaver

--- script/main.js
+++ script/main.js
@@ -77,3 +77,18 @@
                     scrollToPosition(deserts_scroll_height);
                 });
             });
+
+            var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 1000); // Change image every 2 seconds
+}
